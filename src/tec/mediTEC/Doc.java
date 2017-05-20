12fa461dd897faa04dd9a4e395b3890c584929d8
PedroCN97 <pedro1997cn@gmail.com>
2017-05-20 02:39:58 -0600

    Ya puedo hacer los puts

--- src/tec/mediTEC/Doc.java
+++ src/tec/mediTEC/Doc.java
@@ -6,14 +6,18 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.http.HTTPException;
 
+import tec.mediTEC.medicResources.cita;
 import tec.mediTEC.usuarios.Doctor;
 
-@Path("/Doctor")
+@Path("/Doctores")
 public class Doc {
 	
 	private static List<Doctor> doctores = new ArrayList<>();
@@ -31,6 +35,18 @@ public class Doc {
 		}
 	}
 	
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Doctor getDoctor(@PathParam("id") int id){
+		Doctor doc = this.findDoc(id);
+		if (doc!= null){
+			return doc;
+		}else{
+			throw new HTTPException(404);
+		}
+	}
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response newDoctor(Doctor nuevo){
@@ -41,6 +57,49 @@ public class Doc {
 			doctores.add(nuevo);
 			return Response.ok().build();
 		}
+	}
+	
+	@POST
+	@Path("/citas/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void newCita(@PathParam("id") int id, cita nueva){
+		Doctor doc = this.findDoc(id);
+		if (doc != null){
+			doc.getCitas().add(nueva);
+		}		
+	}
+
+	@PUT
+	@Path("/correo/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCorreo(@PathParam("id") int id, String nuevo){
+		Doctor doc = this.findDoc(id);
+		if (doc != null && nuevo != null){
+			doc.setCorreo(nuevo);
+		}
+		
+	}
+	
+	@PUT
+	@Path("/calificacion/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCalificacion(@PathParam("id") int id, int nueva){
+		Doctor doc = this.findDoc(id);
+		if(doc != null){
+			doc.setCalificación(doc.getCalificación() + nueva);
+		}
+		
+	}
+	
+	private Doctor findDoc(int id){
+		for(Doctor i : doctores){
+			if(i.getCodigo() == id){
+				return i;
+			}else{
+				return null;
+			}
+		}
+		return null;
 	}
 
 }
--- src/tec/mediTEC/Pac.java
+++ src/tec/mediTEC/Pac.java
@@ -6,14 +6,17 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.ws.http.HTTPException;
 
 import tec.mediTEC.usuarios.Paciente;
 
-@Path("/Paciente")
+@Path("/Pacientes")
 public class Pac {
 	
 	private static List<Paciente> pacientes = new ArrayList<>();
@@ -31,6 +34,19 @@ public class Pac {
 		}
 	}
 	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Paciente getPaciente(@PathParam("id") int id){
+		Paciente pcite = this.findPaciente(id);
+		if(pcite != null){
+			return pcite;
+		}else{
+			throw new HTTPException(404);
+		}
+		
+	}
+	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response newPaciente(Paciente nuevo){
@@ -43,4 +59,25 @@ public class Pac {
 		}
 	}
 
+	@PUT
+	@Path("/correo/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateCorreo(@PathParam("id") int id, String nuevo){
+		Paciente pcite = this.findPaciente(id);
+		if(pcite != null && nuevo != null){
+			pcite.setCorreo(nuevo);
+		}
+		
+	}
+	
+	private Paciente findPaciente(int id){
+		for(Paciente i : pacientes){
+			if(i.getCodigo() == id){
+				return i;
+			}else{
+				return null;
+			}
+		}
+		return null;
+	}
 }
