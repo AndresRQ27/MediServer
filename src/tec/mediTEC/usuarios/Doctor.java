333af54d0ad72747cc9d5868bbd2e2c3068560f8
PedroCN97 <pedro1997cn@gmail.com>
2017-05-23 00:37:19 -0600

    calificacion

--- src/tec/mediTEC/usuarios/Doctor.java
+++ src/tec/mediTEC/usuarios/Doctor.java
@@ -9,12 +9,12 @@ public class Doctor extends Registro{
 	private String nombre;
 	private String correo;
 	private int codigo;
-	private int calificación = 5;
+	private int calificacion = 5;
 	private static List<cita> citas;//acá va a un árbol
 	
 	public Doctor(){
 		this.codigo = super.numRandom();
-		this.calificación = 0;
+		this.calificacion = 0;
 		citas = new ArrayList<>();//árbol
 		try{
 			super.codigoQR(this.codigo);
@@ -57,11 +57,11 @@ public class Doctor extends Registro{
 	}
 
 	public int getCalificación() {
-		return calificación;
+		return calificacion;
 	}
 
 	public void setCalificación(int calificación) {
-		this.calificación = calificación;
+		this.calificacion = calificación;
 	}
 	
 	
